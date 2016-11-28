499f92e49b6a493061338d8d84ee57d40c002b54
Doniec Mateusz M sta534 <m.doniec@edu.salford.ac.uk>
2016-11-28 16:28:38 +0000

    Clean-up

--- HermesBot/src/HermesBot.java
+++ HermesBot/src/HermesBot.java
@@ -1,8 +1,5 @@
-/**
- * Created by sta534 on 28/11/16.
- */
 public class HermesBot {
     public static void main (String args[]){
-        System.out.println("asd");
+        System.out.println("asd2");
     }
 }
