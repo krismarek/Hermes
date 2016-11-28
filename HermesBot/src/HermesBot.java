74d71171870f3675bf3af3d174f207e2840f6328
FeeVeX <matikile@hotmail.com>
2016-11-28 18:45:27 +0000

    First Test

---
+++ HermesBot/src/HermesBot.java
@@ -0,0 +1,14 @@
+class HermesBot {
+    private static HermesBot ourInstance = new HermesBot();
+
+    public static HermesBot getInstance() {
+        return ourInstance;
+    }
+
+    private HermesBot() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
